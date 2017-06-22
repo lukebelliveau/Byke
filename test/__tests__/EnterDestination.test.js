fbcd288086f6f73e46fa7e9057459c5458e9ab97
lukebelliveau <lbellive@thoughtworks.com>
2017-06-22 16:51:08 -0500

    added inconsequential snapshot test for EnterDestination

--- test/__tests__/EnterDestination.test.js
+++ test/__tests__/EnterDestination.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import EnterDestination from '../../src/EnterDestination';
 
 it('should render', () => {
-  const input = renderer.create(<EnterDestination/>);
+  const input = renderer.create(<EnterDestination />);
 
   expect(input).toMatchSnapshot();
-})
+});
