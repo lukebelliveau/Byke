80178ea3060e94fb17b281ccb47f0bf9d59369ee
lukebelliveau <lbellive@thoughtworks.com>
2017-06-22 20:57:01 -0500

    added flow annots to api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
+// @flow
 export const url = 'http://byke-graphql.herokuapp.com/graphql?query=';
 
-const getAllStations = stationParams =>
+const getAllStations = (stationParams: string) =>
   fetch(`${url}{allStations${stationParams}}`)
     .then(response => {
       return response.json().then(res => {
