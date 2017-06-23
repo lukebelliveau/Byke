9a81cda67e3170f551531b3000c25cfb2d5ed57b
lukebelliveau <lbellive@thoughtworks.com>
2017-06-23 17:22:12 -0500

    Location card looks how I want, need to turn it into a scrollview

---
+++ src/LocationList.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+
+const LocationList = ({ result }) =>
+  <View style={styles.container}>
+    <LocationCard result={result} />
+  </View>;
+
+const LocationCard = ({ result }) =>
+  <View style={styles.card}>
+    <View style={styles.infoContainer}>
+      <Info name={result.name} address={result.vicinity} />
+    </View>
+    <View style={styles.buttonContainer}>
+      <Button />
+    </View>
+  </View>;
+
+const Button = ({ onPress }) =>
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.buttonText}>
+      GO
+    </Text>
+  </TouchableOpacity>;
+
+const Info = ({ name, address }) =>
+  <View style={{ flex: 1 }}>
+    <Text style={styles.title}>{name}</Text>
+    <Text style={styles.address}>{address}</Text>
+  </View>;
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    display: 'flex',
+    top: 0,
+    width: '100%',
+    height: '100%',
+    marginRight: '10%',
+    paddingLeft: '5%',
+    paddingRight: '5%',
+  },
+  card: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    height: '20%',
+    backgroundColor: 'white',
+  },
+  infoContainer: {
+    flex: 4,
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
+  title: {
+    fontSize: 40,
+  },
+  address: {
+    position: 'absolute',
+    bottom: 10,
+    fontSize: 15,
+    width: '75%',
+    color: 'gray',
+  },
+  buttonContainer: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+  },
+  button: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'ghostwhite',
+  },
+  buttonText: {
+    fontSize: 40,
+    color: 'deepskyblue',
+  },
+});
+
+export default LocationList;
