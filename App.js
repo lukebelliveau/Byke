0f612af10bf651a880c8c15db1ab0b997b147b62
lukebelliveau <lbellive@thoughtworks.com>
2017-06-27 13:47:08 -0500

    set up debugger

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-
 import Byke from './src/components/Byke';
 import reducers from './src/redux/reducers';
 import searchLocationSaga from './src/redux/searchPlacesSaga';
 
 const sagaMiddleware = createSagaMiddleware();
-let store = createStore(reducers, applyMiddleware(sagaMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(sagaMiddleware)))
 sagaMiddleware.run(searchLocationSaga);
 
 const BykeApp = () =>
